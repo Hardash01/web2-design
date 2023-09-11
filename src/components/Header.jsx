47befaa408e6cf4f2d0c83abaeae2d8696076b3f
Hardash01 <edwin98santos@gmail.com>
2023-09-11 09:38:53 -0600

    modificacion logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,7 +17,7 @@ const Header = () => {
                 <a href='#' className="flex items-center py-2">
                     <img
                         className="h-12 w-auto"
-                        src="/public/logo.png"
+                        src="/logo.png"
                         alt="logo"
                     />
                 </a>
